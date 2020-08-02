408475ab09a064b09aee83578e9992072f912037
kp047i <takayuki.miura28@gmail.com>
2020-08-03 01:19:29 +0900

    chore: rm common style

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="wrapper">
     <TheHeader />
     <Nuxt />
   </div>
@@ -15,17 +15,3 @@ export default Vue.extend({
   },
 })
 </script>
-
-<style>
-html {
-  font-family: 'Noto Sans JP', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
-</style>
