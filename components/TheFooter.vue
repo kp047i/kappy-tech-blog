b303cd0199c703e372cf7ae89203800fa5f22793
kp047i <takayuki.miura28@gmail.com>
2020-08-04 00:19:02 +0900

    style: add footer

---
+++ components/TheFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="bg-navy w-full px-4 py-4">
+    <p class="p-2 text-center text-xs text-cream">
+      &copy; 2020 <a href="https://github.com/kp047i">kappy</a>
+    </p>
+  </footer>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({})
+</script>
