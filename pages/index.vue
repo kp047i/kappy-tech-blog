8221e90eef1bfb152dac8c5a38fb869c2a8b93f2
kp047i <takayuki.miura28@gmail.com>
2020-10-26 23:04:27 +0900

    feat: add article card component

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
-  <div class="py-8">
-    <div v-for="article in articles" :key="article.title">
-      <nuxt-link :to="article.slug">
-        {{ article.title }}
-      </nuxt-link>
+  <main class="flex-grow px-8 container mx-auto pt-16">
+    <div class="py-8 flex flex-wrap">
+      <div
+        v-for="article in articles"
+        :key="article.title"
+        class="p-4 md:w-1/2 lg:w-1/3"
+      >
+        <nuxt-link :to="article.slug">
+          <ArticleCard :article="article" />
+        </nuxt-link>
+      </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import ArticleCard from '@/components/ArticleCard.vue'
 
 export default Vue.extend({
+  components: {
+    ArticleCard,
+  },
   async asyncData({ $content }) {
-    const articles = await $content().sortBy('position', 'desc').fetch()
+    const articles = await $content()
+      .where({ slug: { $ne: 'privacy-policy' } })
+      .sortBy('position', 'desc')
+      .fetch()
     return {
       articles,
     }
