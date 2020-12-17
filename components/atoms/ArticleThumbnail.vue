0e0c7e555fd834123ef9bcd4ebe6e728cacf6f22
kp047i <takayuki.miura28@gmail.com>
2020-12-17 23:17:52 +0900

    feat: add webp

---
+++ components/atoms/ArticleThumbnail.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="py-8">
+    <picture>
+      <source :srcset="`/img/ogp/${slug}.webp`" type="image/webp" />
+      <img :src="`/img/ogp/${slug}.png`" :alt="`${slug} thumbnail`" />
+    </picture>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  props: {
+    slug: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
