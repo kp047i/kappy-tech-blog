cd5652f2bb378d49497812dd4654a0ad74e2c326
kp047i <takayuki.miura28@gmail.com>
2020-12-17 23:16:57 +0900

    chore: add vue-a11y rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,8 +10,8 @@ module.exports = {
     'prettier/vue',
     'plugin:prettier/recommended',
     'plugin:nuxt/recommended',
+    'plugin:vue-a11y/base',
   ],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'vue-a11y'],
   // add your custom rules here
-  rules: {},
 }
