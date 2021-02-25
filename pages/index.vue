368d34c8f30d6e650739662cc8350615d51c23d4
kp047i <takayuki.miura28@gmail.com>
2021-02-25 16:29:15 +0900

    a11y: add aria-label to update icon

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,15 @@
 <template>
   <main
-    class="flex-grow container mx-auto max-w-screen-xl px-2 sm:px-4 md:px-4 pt-16"
+    class="container flex-grow max-w-screen-xl px-2 pt-16 mx-auto sm:px-4 md:px-4"
   >
+    <h1 class="mt-8 text-2xl lg:px-16">記事一覧</h1>
     <div
-      class="flex flex-wrap justify-center md:justify-start py-4 md:py-8 lg:px-8"
+      class="flex flex-wrap justify-center py-4 md:justify-start md:py-8 lg:px-8"
     >
       <div
         v-for="article in articles"
         :key="article.title"
-        class="md:w-1/2 xl:w-1/3 p-2 md:p-4 lg:px-10 xl:px-4 mb-2"
+        class="p-2 mb-2 md:w-1/2 xl:w-1/3 md:p-4 lg:px-10 xl:px-4"
       >
         <nuxt-link :to="`${article.slug}/`">
           <ArticleCard :article="article" />
